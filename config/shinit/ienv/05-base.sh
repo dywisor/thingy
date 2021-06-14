5d648a1a7d745034da66ec92eebcbe3706df01c4
André Erdmann <dywi@mailerd.de>
2021-06-14 20:10:48 +0200

    shinit: add __genret__

--- config/shinit/ienv/05-base.sh
+++ config/shinit/ienv/05-base.sh
@@ -6,6 +6,8 @@ __nostderr__() { "${@}" 2>/dev/null; }
 __quietly__()  { "${@}" 1>/dev/null 2>&1; }
 __have_cmd__() { __quietly__ command -V "${1}";  }
 
+__genret__()   { return "${1?}"; }
+
 
 newdir() {
     [ -n "${1-}" ] && mkdir -p -- "${1}" && cd -- "${1}"
